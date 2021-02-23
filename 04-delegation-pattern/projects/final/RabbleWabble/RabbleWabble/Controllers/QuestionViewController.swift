da1fbd15a729116027558330090e989b66e4be69
taelee <taelee@student.42seoul.kr>
2021-02-23 14:18:49 +0900

    04-delegation-pattern: - RabbleWabble 1차 완료 - Creating a custom delegate 하기 직전

--- 04-delegation-pattern/projects/final/RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
+++ 04-delegation-pattern/projects/final/RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
@@ -28,30 +28,10 @@
 
 import UIKit
 
-public protocol QuestionViewControllerDelegate: class {
-  
-  // 1
-  func questionViewController(
-    _ viewController: QuestionViewController,
-    didCancel questionGroup: QuestionGroup,
-    at questionIndex: Int)
-  
-  // 2
-  func questionViewController(
-    _ viewController: QuestionViewController,
-    didComplete questionGroup: QuestionGroup)
-}
-
 public class QuestionViewController: UIViewController {
 
   // MARK: - Instance Properties
-  public weak var delegate: QuestionViewControllerDelegate?
-  
-  public var questionGroup: QuestionGroup! {
-    didSet {
-      navigationItem.title = questionGroup.title
-    }
-  }
+  public var questionGroup = QuestionGroup.basicPhrases()
   public var questionIndex = 0
   
   public var correctCount = 0
@@ -62,39 +42,10 @@ public class QuestionViewController: UIViewController {
     return (view as! QuestionView)
   }
   
-  private lazy var questionIndexItem: UIBarButtonItem = {
-    let item = UIBarButtonItem(title: "",
-                               style: .plain,
-                               target: nil,
-                               action: nil)
-    item.tintColor = .black
-    navigationItem.rightBarButtonItem = item
-    return item
-  }()
-  
   // MARK: - View Lifecycle
   public override func viewDidLoad() {
     super.viewDidLoad()
-    setupCancelButton()
     showQuestion()
-  }
-  
-  private func setupCancelButton() {
-    let action = #selector(handleCancelPressed(sender:))
-    let image = UIImage(named: "ic_menu")
-    navigationItem.leftBarButtonItem =
-      UIBarButtonItem(image: image,
-                      landscapeImagePhone: nil,
-                      style: .plain,
-                      target: self,
-                      action: action)
-  }
-  
-  @objc private func handleCancelPressed(sender: UIBarButtonItem) {
-    delegate?.questionViewController(
-      self,
-      didCancel: questionGroup,
-      at: questionIndex)
   }
   
   private func showQuestion() {
@@ -106,8 +57,6 @@ public class QuestionViewController: UIViewController {
     
     questionView.answerLabel.isHidden = true
     questionView.hintLabel.isHidden = true
-    questionIndexItem.title = "\(questionIndex + 1)/" +
-    "\(questionGroup.questions.count)"
   }
   
   // MARK: - Actions
@@ -133,7 +82,7 @@ public class QuestionViewController: UIViewController {
   private func showNextQuestion() {
     questionIndex += 1
     guard questionIndex < questionGroup.questions.count else {
-      delegate?.questionViewController(self, didComplete: questionGroup)
+      // TODO: - Handle this...!
       return
     }
     showQuestion()
--- 04-delegation-pattern/projects/final/RabbleWabble/RabbleWabble/Controllers/SelectQuestionGroupViewController.swift
+++ 04-delegation-pattern/projects/final/RabbleWabble/RabbleWabble/Controllers/SelectQuestionGroupViewController.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2019 Razeware LLC
+/// Copyright (c) 2021 Razeware LLC
 /// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -29,76 +29,52 @@
 import UIKit
 
 public class SelectQuestionGroupViewController: UIViewController {
-  
-  // MARK: - Outlets
-  @IBOutlet internal var tableView: UITableView! {
-    didSet {
-      tableView.tableFooterView = UIView()
+
+    //이렇게 하면 아래 불필요한 셀을 안보이게 할 수 있음
+    // MARK: - Outlets
+    @IBOutlet internal var tableView: UITableView! {
+        didSet {
+            tableView.tableFooterView = UIView()
+        }
     }
-  }
-  
-  // MARK: - Properties
-  public let questionGroups = QuestionGroup.allGroups()
-  private var selectedQuestionGroup: QuestionGroup!
+
+    // MARK: - Properties
+    public let questionGroups = QuestionGroup.allGroups()
+    private var selectedQuestionGroup: QuestionGroup!
 }
 
 // MARK: - UITableViewDataSource
 extension SelectQuestionGroupViewController: UITableViewDataSource {
-  
-  public func tableView(_ tableView: UITableView,
-                        numberOfRowsInSection section: Int)
-    -> Int {
-      return questionGroups.count
-  }
-  
-  public func tableView(_ tableView: UITableView,
-                        cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionGroupCell") as! QuestionGroupCell
-      let questionGroup = questionGroups[indexPath.row]
-      cell.titleLabel.text = questionGroup.title
-      return cell
-  }
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questionGroups.count
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionGroupCell") as! QuestionGroupCell
+        let questionGroup = questionGroups[indexPath.row]
+        cell.titleLabel.text = questionGroup.title
+        return cell
+    }
 }
 
+// MARK: - UITableViewDelegate
 extension SelectQuestionGroupViewController: UITableViewDelegate {
-  
-  public func tableView(_ tableView: UITableView,
-                        willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-      selectedQuestionGroup = questionGroups[indexPath.row]
-      return indexPath
-  }
-  
-  public func tableView(_ tableView: UITableView,
-                        didSelectRowAt indexPath: IndexPath) {
-    tableView.deselectRow(at: indexPath, animated: true)
-  }
-  
-  public override func prepare(for segue: UIStoryboardSegue,
-                               sender: Any?) {
-    guard let viewController = segue.destination
-      as? QuestionViewController else { return }
-    viewController.questionGroup = selectedQuestionGroup
-    viewController.delegate = self
-  }
-}
+    // 1
+    public func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        selectedQuestionGroup = questionGroups[indexPath.row]
+        return indexPath
+    }
 
-// MARK: - QuestionViewControllerDelegate
-extension SelectQuestionGroupViewController: QuestionViewControllerDelegate {
-  
-  public func questionViewController(
-    _ viewController: QuestionViewController,
-    didCancel questionGroup: QuestionGroup,
-    at questionIndex: Int) {
-    
-    navigationController?.popToViewController(self,
-                                              animated: true)
-  }
-  
-  public func questionViewController(
-    _ viewController: QuestionViewController,
-    didComplete questionGroup: QuestionGroup) {
-    
-    navigationController?.popToViewController(self,
-                                              animated: true)
-  }
+    // 2
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    // 3
+    public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let viewController = segue.destination as? QuestionViewController else {
+            return
+        }
+        viewController.questionGroup = selectedQuestionGroup
+    }
 }
